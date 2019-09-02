9f9761e184227e80e56953f0b5cc33a67c68f92a
Alma <2484094776qq.com>
2019-09-02 20:07:11 +0800

    test

---
+++ Project1/Project1/源.c
@@ -0,0 +1,29 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<stdlib.h>
+//void printN(int N)
+//{
+//	int i;
+//	for(i=1;i<=N;i++)
+//	{
+//		printf("%d ", i);
+//	}
+//	return;
+//}
+void printN(int N)
+{
+	if (N)
+	{
+		printN(N - 1);
+		printf("%d ", N);
+	}
+	return;
+}
+int main()
+{
+	int N;
+	scanf("%d", &N);
+	printN(N);
+	system("pause");
+	return 0;
+}
